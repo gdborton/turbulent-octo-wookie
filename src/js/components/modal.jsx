11b42557a127d3c77c2f60911f051ea21be2c822
Gary Borton <gborton@exacttarget.com>
2015-09-04 02:26:19 -0400

    Make clicking the backdrop of a modal close the modal.

--- src/js/components/modal.jsx
+++ src/js/components/modal.jsx
@@ -3,12 +3,17 @@ var React = require('react');
 var Modal = React.createClass({
   render() {
     return (
-      <div className="modal-backdrop">
+      <div className="modal-backdrop" onClick={this._handleBackdropClick}>
         <div className="modal-contents">
           {this.props.children}
         </div>
       </div>
     );
+  },
+  _handleBackdropClick(event) {
+    if(event.target === event.currentTarget) {
+      this.props.onClose();
+    }
   }
 });
 var modalNode = document.querySelector('.modal-node');
@@ -17,7 +22,7 @@ var appContainer = document.querySelector('body > .container');
 var MockModal = React.createClass({
   componentWillMount() {
     appContainer.className = appContainer.className + ' blur';
-    React.render(<Modal>{this.props.children}</Modal>, modalNode);
+    React.render(<Modal onClose={this.props.onClose}>{this.props.children}</Modal>, modalNode);
   },
   componentWillUnmount() {
     appContainer.className = appContainer.className.replace(/blur/, '');
