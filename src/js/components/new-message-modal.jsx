d63e5c6fe35c23b3959aba51464c4d9cb7775452
Gary Borton <gborton@exacttarget.com>
2015-09-05 01:30:27 -0400

    Small style tweaks + add photo on new message modal.

--- src/js/components/new-message-modal.jsx
+++ src/js/components/new-message-modal.jsx
@@ -7,11 +7,11 @@ var NewMessageModal = React.createClass({
     return (
       <Modal onClose={this.props.onClose}>
         <div className='new-message-container'>
-          <div>Create new message</div>
-          <textarea ref="input" rows={3}></textarea>
+          <div className="new-message-container-header">Create new message</div>
+          <textarea ref="input" rows={4}></textarea>
           <div>
-            Add Photo
-            Add Video
+            <span className="add-photo dark" onClick={this._addPhoto}><span className="wide-icon photo-icon-dark"></span> Add Photo</span>
+            <span className="add-video dark"><span className="wide-icon video-icon-dark"></span> Add Video</span>
             <button onClick={this._handleClickPost}>Post</button>
           </div>
         </div>
@@ -24,6 +24,11 @@ var NewMessageModal = React.createClass({
       postActions.addPost(node.value);
       this.props.onClose();
     }
+  },
+  _addPhoto() {
+    var node = this.refs.input.getDOMNode();
+    node.value = node.value + ' http://instagram.com/p/fake ';
+    node.focus();
   }
 });
 
