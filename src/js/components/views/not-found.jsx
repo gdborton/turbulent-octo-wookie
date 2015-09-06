5d5fabc26511fab4978bacba8f5bbad3e97ffce8
Gary Borton <gborton@exacttarget.com>
2015-09-06 00:44:19 -0400

    Add a bit more content to the not found page.

--- src/js/components/views/not-found.jsx
+++ src/js/components/views/not-found.jsx
@@ -3,7 +3,17 @@ var React = require('react');
 var NotFound = React.createClass({
   render() {
     return (
-      <div>Not Found</div>
+      <div className="small container">
+        <h1>Well that's embarassing...</h1>
+        <p>
+          Due to time limitations, not all pages were built out, and this is one of the pages that didn't make the cut.
+        </p>
+        <p>
+          Hopefully you can enjoy this relevant 1987 music video, as much as the content you expected to see.
+        </p>
+        <h2>Rick Astley's "Never Gonna Give You Up"</h2>
+        <iframe width="480" height="360" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" frameborder="0" allowfullscreen></iframe>
+      </div>
     );
   }
 });
--- src/js/components/views/settings.jsx
+++ src/js/components/views/settings.jsx
@@ -14,36 +14,34 @@ var Settings = React.createClass({
     ];
 
     return (
-      <div>
-        <div className="small container">
-          <h1>Settings</h1>
-          <Section title="Account">
-            <div className="profile-picture-container">
-              <div className="profile-picture">
-                <img src="http://lorempixel.com/85/85/people/?author=Me"/>
-              </div>
-              <button className="secondary change-picture">change</button>
+      <div className="small container">
+        <h1>Settings</h1>
+        <Section title="Account">
+          <div className="profile-picture-container">
+            <div className="profile-picture">
+              <img src="http://lorempixel.com/85/85/people/?author=Me"/>
             </div>
-            <div className="account-settings">
-              <FancyInput leftContent={<span className="icon person-icon"/>} />
-              <FancyInput leftContent={<span className="icon mail-icon"/>} />
-              <FancyInput leftContent={<span className="icon lock-icon"/>} rightContent='Change' type='password'/>
-            </div>
-          </Section>
-          <Section title="Notifications">
-            <ToggleSwitch>email me when my posts are marked as favorites</ToggleSwitch>
-            <ToggleSwitch>email me when I'm mentioned</ToggleSwitch>
-            <ToggleSwitch>email me when I get a reply</ToggleSwitch>
-            <ToggleSwitch>email me when someone follows me</ToggleSwitch>
-          </Section>
-          <Section title="Privacy">
-            <RadioSwitches name="privacy" options={options}/>
-            <ToggleSwitch>add a location to my posts</ToggleSwitch>
-            <ToggleSwitch>let others find me by my email address</ToggleSwitch>
-            <ToggleSwitch>tailor ads based on my information</ToggleSwitch>
-          </Section>
-          <button>Save Changes</button>
-        </div>
+            <button className="secondary change-picture">change</button>
+          </div>
+          <div className="account-settings">
+            <FancyInput leftContent={<span className="icon person-icon"/>} />
+            <FancyInput leftContent={<span className="icon mail-icon"/>} />
+            <FancyInput leftContent={<span className="icon lock-icon"/>} rightContent='Change' type='password'/>
+          </div>
+        </Section>
+        <Section title="Notifications">
+          <ToggleSwitch>email me when my posts are marked as favorites</ToggleSwitch>
+          <ToggleSwitch>email me when I'm mentioned</ToggleSwitch>
+          <ToggleSwitch>email me when I get a reply</ToggleSwitch>
+          <ToggleSwitch>email me when someone follows me</ToggleSwitch>
+        </Section>
+        <Section title="Privacy">
+          <RadioSwitches name="privacy" options={options}/>
+          <ToggleSwitch>add a location to my posts</ToggleSwitch>
+          <ToggleSwitch>let others find me by my email address</ToggleSwitch>
+          <ToggleSwitch>tailor ads based on my information</ToggleSwitch>
+        </Section>
+        <button>Save Changes</button>
       </div>
     );
   }
