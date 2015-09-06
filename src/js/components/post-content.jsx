6746a8b6434bd2126f564f67b7038e212988c775
Gary Borton <gborton@exacttarget.com>
2015-09-06 00:34:29 -0400

    Add twitter handle matching to post-content.

--- src/js/components/post-content.jsx
+++ src/js/components/post-content.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 
 var urlRegex = /[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
+var twitterHandleRegex = /^@(\w){1,15}/;
 
 var PostContent = React.createClass({
   render() {
@@ -8,6 +9,8 @@ var PostContent = React.createClass({
     content = content.map(function(word, index) {
       if (urlRegex.test(word)) {
         return <span><a href={word.indexOf('http') === -1 ? 'https://' + word : word} key={index}>{word}</a> </span>;
+      } else if(twitterHandleRegex.test(word)) {
+        return <span><a href={'https://twitter.com/' + word.match(twitterHandleRegex)[0].replace(/@/, '')} key={index}>{word}</a></span>
       } else {
         return word + ' ';
       }
